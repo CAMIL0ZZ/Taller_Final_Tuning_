190e78d9cded6a47f979fb6d16be8d85b8d9440c
CAMIL0ZZ <accontreras94@ucatolica.edu.co>
2026-06-04 01:11:10 -0500

    [create] subida de ajustes db

---
+++ config/settings.py
@@ -0,0 +1,14 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+
+    SUPABASE_URL: str
+    SUPABASE_KEY: str
+    SUPABASE_BUCKET: str
+
+    class Config:
+        env_file = ".env"
+
+
+settings = Settings()
